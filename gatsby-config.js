96d5d713d69c2a37420bcd7fe23b4bb1bcbfe2c8
soaresandrie <jsoaresandrie@gmail.com>
2020-02-11 23:32:09 -0300

    add arquivo de config do gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  siteMetadata: {
+    title: "Lima Móveis Planejados"
+  },
+
+  plugins: [
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-postcss",
+    {
+      resolve: "gatsby-plugin-purgecss",
+      options: {
+        printRejected: true,
+        tailwind: true
+      }
+    }
+  ]
+};
